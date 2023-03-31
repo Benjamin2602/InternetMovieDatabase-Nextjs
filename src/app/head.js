a04b8c5ec8afd1371decfb5f2b48a23fb05242ea
Benjamin2602 <91728550+Benjamin2602@users.noreply.github.com>
2023-03-31 18:55:28 +0530

    install nextjs

---
+++ src/app/head.js
@@ -0,0 +1,9 @@
+export default function head(){
+    return(
+        <>
+            <title>imdb clone</title>
+            <meta content="width=device-width, initial-scale=1" name="viewport"/>
+            <meta name="description" content="This is an IMDB clone"/>
+        </>
+    )
+}
